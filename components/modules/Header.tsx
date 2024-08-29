a73eb5938526992502ca3602f4b3c7bb7fada3c7
sobhan-yaghobi <sobhan.yaghobi.work@gmail.com>
2024-08-29 10:02:44 +0330

    build => initial project And add themes And build header

---
+++ components/modules/Header.tsx
@@ -0,0 +1,38 @@
+import React from "react"
+
+import GithubWhiteIcon from "@/public/github-mark-white.svg"
+import GithubIcon from "@/public/github-mark.svg"
+
+import ChangeThemeToggle from "./ChangeThemeToggle"
+import Image from "next/image"
+
+const Header: React.FC = () => {
+  return (
+    <header className="h-20 border-b-[.5px]">
+      <div className="container h-full flex justify-between items-center">
+        <div className="flex items-center">
+          <div className="size-12 center relative">
+            <Image
+              width={40}
+              height={40}
+              src={GithubIcon}
+              className="absolute dark:opacity-0"
+              alt="github-icon"
+            />
+            <Image
+              width={40}
+              height={40}
+              src={GithubWhiteIcon}
+              className="absolute opacity-0 dark:opacity-100"
+              alt="github-icon"
+            />
+          </div>
+          <h3 className="text-xl font-extrabold ml-3">Github</h3>
+        </div>
+        <ChangeThemeToggle />
+      </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ components/providers/ThemeProvider.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import React from "react"
+import { ThemeProvider as NextThemeProvider } from "next-themes"
+import { type ThemeProviderProps } from "next-themes/dist/types"
+
+const ThemeProvider: React.FC<ThemeProviderProps> = ({ children, ...props }) => {
+  return <NextThemeProvider {...props}>{children}</NextThemeProvider>
+}
+
+export default ThemeProvider
