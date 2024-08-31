c4b6aca82107063d5332379ee0e3b80a71b54921
sobhan-yaghobi <sobhan.yaghobi.work@gmail.com>
2024-08-31 10:25:07 +0330

    update => background login style

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,10 +7,11 @@ const page: React.FC<SearchParamsComponentProps> = ({ searchParams }) => {
   return (
     <div className="w-svw h-screen center relative overflow-hidden">
       <LoginForm code={code} />
-      <div className="ellipse w-[150vw] max-lg:w-[240vw]">
-        <div className="rec1" />
-        <div className="rec2" />
-        <div className="rec3" />
+
+      <div className="ellipse-background h-screen w-[150vw] rounded-[100%] top-1/2 center absolute max-lg:w-[240vw]">
+        <div className="absolute -z-10 top-0 size-[550px] bg-[#7d12ff] blur-[100px] rotate-45" />
+        <div className="absolute -z-10 size-[725px] bg-[#764ba6] blur-[200px] rotate-45" />
+        <div className="absolute -z-10 size-[600px] bg-[#7480ff] blur-[300px] rotate-45" />
       </div>
     </div>
   )
